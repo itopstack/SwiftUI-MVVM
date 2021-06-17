9b3b0d8aec1f630b7e99784c1d8f1e647a331d6c
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2021-06-17 15:39:35 +0700

    Add files

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Kittisak Phetrungnapha on 17/6/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
